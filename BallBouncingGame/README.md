c01a39c6e2bb9490da501a8c4af02d23ed2901c1
kschniedergers <kschniedergers@users.noreply.github.com>
2017-05-01 01:27:38 -0700

    Add files via upload

---
+++ BallBouncingGame/README.md
@@ -0,0 +1,2 @@
+# BallBouncingGame
+WIP
---
+++ html/src/com/kaigdx/game1/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.kaigdx.game1.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.kaigdx.game1.KaiGDXGame1;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new KaiGDXGame1();
+        }
+}
