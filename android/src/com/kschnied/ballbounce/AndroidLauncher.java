c5f8af26fea9836dd29070ff788d16f22605180e
Kai Schniedergers <kschnied@ucsc.edu>
2017-05-01 12:17:26 -0700

    Base Libgdx

---
+++ android/src/com/kschnied/ballbounce/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kschnied.ballbounce;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kschnied.ballbounce.BallBounce;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BallBounce(), config);
+	}
+}
---
+++ desktop/src/com/kschnied/ballbounce/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kschnied.ballbounce.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kschnied.ballbounce.BallBounce;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BallBounce(), config);
+	}
+}
